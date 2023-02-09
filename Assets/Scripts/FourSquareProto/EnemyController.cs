596fd14b48e02eec1a9074eb52eaa43eea2bc4c9
JD Calvelli <jdcalvelli@gmail.com>
2023-02-08 20:23:12 -0500

    first pass integration

--- Assets/Scripts/FourSquareProto/EnemyController.cs
+++ Assets/Scripts/FourSquareProto/EnemyController.cs
@@ -6,28 +6,34 @@ using Random = UnityEngine.Random;
 
 public class EnemyController : Competitor
 {
+    [SerializeField] private BeatManager beatManager;
+    
     public override void HitBallTo(Action<BallController.BallLocation> callback)
     {
-        // todo have to 
+        // todo - have to change it such that the enemy cant send it to themselves
+        // todo - have to make it so that the enemies only make a shot once within an interval
         
-        int shotChooser = Random.Range(0, 4);
-        switch (shotChooser)
+        if (beatManager.OnBeat())
         {
-            case 0:
-                callback(BallController.BallLocation.Player);
-                break;
+            int shotChooser = Random.Range(0, 4);
+            switch (shotChooser)
+            {
+                case 0:
+                    callback(BallController.BallLocation.Player);
+                    break;
             
-            case 1:
-                callback(BallController.BallLocation.Enemy1);
-                break;
+                case 1:
+                    callback(BallController.BallLocation.Enemy1);
+                    break;
             
-            case 2:
-                callback(BallController.BallLocation.Enemy2);
-                break;
+                case 2:
+                    callback(BallController.BallLocation.Enemy2);
+                    break;
             
-            case 3:
-                callback(BallController.BallLocation.Enemy3);
-                break;
+                case 3:
+                    callback(BallController.BallLocation.Enemy3);
+                    break;
+            }
         }
     }
 }
