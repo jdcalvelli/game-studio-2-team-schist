dccbf99b96d1b389957fc5a64699f8e14894fa51
tommygents <tmaranges+gh@gmail.com>
2023-02-10 18:50:44 -0500

    Added code to determine whether we've entered a new interval, and handles permission that way. Also cleaned up code comments.

--- Assets/Scripts/FourSquareProto/BeatManager.cs
+++ Assets/Scripts/FourSquareProto/BeatManager.cs
@@ -7,47 +7,54 @@ public class BeatManager : MonoBehaviour
 
     public int BPM = 120;
 
-    // this means that the tolerance is 12.5% of the interval between beats, i.e. 1/8th of a note on either side. So it's a 1/4-long tolerance
+    // tolerance is a percentage of the interval size; toleranceNum will be the actual size of the interval
+
     public float tolerance = .125f;
     public float interval;
+    public float toleranceNum;
 
     //Lower and upper bound are the timestamps (as a portion of the interval) that a cue needs to be in in order to trigger
     public float lowerBound;
     public float upperBound;
 
-   
-
+    // the last interval that was acted on
+    public int lastInterval;
 
     //OnBeat returns 'true' if it's called in a frame that is within the tolerance window on either side of the beat.
     public bool OnBeat()
     {
         bool b = false;
-        float _ipart = GetIntervalPart();
-        if (_ipart > lowerBound || _ipart < upperBound) 
+        int _thisInterval = GetIntervalBeatCount();
+        if (lastInterval < _thisInterval)
         {
-            b = true;
+            float _ipart = GetIntervalPart();
+            if (_ipart > lowerBound || _ipart < upperBound)
+            {
+                b = true;
+            }
+            lastInterval= _thisInterval;
         }
-
         return b;
 
     }
 
+    //Returns the current distance into the active interval as a fraction of the interval
     public float GetIntervalPart()
     {
-        //Calculates how far into the "interval" we are by taking the fractional part of the current interval; the returned value is as a portion of the interval, e.g. .12 is 12% 
-        float _t = Time.time;
-        int _seconds = (int)_t;
-        float _part = _t - _seconds;
+        float _t = Time.time/interval;
+        int _ipart = (int)_t;
+        float _fpart = _t - _ipart;
         
-        return _part;
+        return _fpart;
     }
 
-    //Get Interval returns which beat we are in. It subtracts tolerance so that the interval doesn't fall in the middle of the window
-    public int GetInterval()
+    //Returns a count of intervals that have passed for the purposes of determining whether we're allowed to act yet
+    public int GetIntervalBeatCount()
     {
-        float _t = Time.time - tolerance;
-        int _seconds = (int)_t;
-        return _seconds;
+        //adds toleranceNum so that back-half of an interval doesn't get treated as the same as the front-half.
+        float _t = (Time.time + toleranceNum)/interval;
+        int _ipart = (int)_t;
+        return _ipart;
     }
 
 
@@ -57,6 +64,8 @@ public class BeatManager : MonoBehaviour
         interval = 60 / BPM;
         lowerBound = 1 - tolerance;
         upperBound = tolerance;
+        toleranceNum = tolerance * interval;
+        lastInterval = 0;
     }
    
 
