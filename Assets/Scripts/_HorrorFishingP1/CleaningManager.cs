390ba5b2a1cc9648b16f93e3c9149130d924eb58
ricottakase <74511410+ricottakase@users.noreply.github.com>
2023-03-03 18:30:36 -0500

    Architecture template for cleaning manager finished

--- Assets/Scripts/_HorrorFishingP1/CleaningManager.cs
+++ Assets/Scripts/_HorrorFishingP1/CleaningManager.cs
@@ -12,7 +12,8 @@ public class CleaningManager : MonoBehaviour
         sliceBelly,
         pullInnards,
         shaveScales,
-        storeInCooler
+        storeInCooler,
+        endSubGame;
     }
 
     private cleaningSubGameStates _cleaningSubGameState = cleaningSubGameStates.startSubGame;
@@ -34,27 +35,42 @@ public class CleaningManager : MonoBehaviour
                         Debug.Log("The fish stares back with a hook through its mouth");
                         _cleaningSubGameState = cleaningSubGameStates.unhookFish;
                 }
+                break;
 
             case (cleaningSubGameStates.unhookFish):
                 UnhookFish();
+                break;
             
             case (cleaningSubGameStates.pickUpKnife):
+            // Maybe exclude this state and add the animation to the start of the subgame or the unhooking ?
                 if (inputManager.PrimaryKeyDown()) {
                     //Play animation of knife being picked up from offscreen *here*
+                    Debug.Log("Knife has been picked up");
                     _cleaningSubGameState = cleaningSubGameStates.cutHead;
                 }
+                break;
 
             case (cleaningSubGameStates.cutHead):
                 CutHead();
+                break;
 
             case (cleaningSubGameStates.sliceBelly):
                 SliceBelly();
+                break;
 
             case (cleaningSubGameStates.pullInnards):
+                PullInnards();
+                break;
 
             case (cleaningSubGameStates.shaveScales):
+                ShaveScales();
+                break;
 
             case (cleaningSubGameStates.storeInCooler):
+                StoreInCooler();
+                _cleaningSubGameState = cleaningSubGameStates.endSubGame;
+                break;
+                //end subgame
         }
     }
 
@@ -87,8 +103,38 @@ public class CleaningManager : MonoBehaviour
             //Play belly slicing animation from caught fish view *here*
             Debug.Log("Its belly has been sliced open.");
 
-            // ""
+            // Move state change outside after fleshing out
             _cleaningSubGameState = cleaningSubGameStates.pullInnards;
+        }
+    }
+
+    private void PullInnards() {
+        // Add pull innards logic
+        if (inputManager.PrimaryKeyDown()) {
+            //Play innard pulling animation from caught fish view *here*
+            Debug.Log("The fish has been gutted.");
+
+            // Move state change outside after fleshing out
+            _cleaningSubGameState = cleaningSubGameStates.shaveScales;
+        }
+    }
+
+    private void ShaveScales() {
+        // Add scale shaving logic
+        if (inputManager.PrimaryKeyDown()) {
+            //Play scale shaving animation from caught fish view *here*
+            Debug.Log("Scales have been carved off the fish.");
+
+            // Move state change outside after fleshing out
+            _cleaningSubGameState = cleaningSubGameStates.storeInCooler;
+        }
+    }
+
+    private void StoreInCooler() {
+        // Add logic for storing fish in cooler
+        if (inputManager.PrimaryKeyDown()) {
+            //Play store in cooler animation from caught fish view *here*
+            Debug.Log("Fish has been stored in the cooler");
         }
     }
 }
