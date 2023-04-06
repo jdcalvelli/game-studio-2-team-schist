bf57cd286114b012a8f6b5a8986c76b2847b4043
JD Calvelli <jdcalvelli@gmail.com>
2023-04-05 22:31:57 -0400

    updated rod view utilizing onBeat event rhythm up

--- Assets/Scripts/_HorrorFishingP1/Fishing/FishingViews/FishingRodView.cs
+++ Assets/Scripts/_HorrorFishingP1/Fishing/FishingViews/FishingRodView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,11 @@ using DG.Tweening;
 
 public class FishingRodView : MonoBehaviour
 {
+    
+    // get sub elements
+    [SerializeField] private Transform handle;
+    [SerializeField] private SpriteRenderer beatCircle;
+    
     public float rodCastTimer = 1f;
     public float lineFlyTimer = 0.1f;
 
@@ -26,5 +32,15 @@ public class FishingRodView : MonoBehaviour
 
     public void Animate_FishIsBiting() {
         transform.DOShakePosition(1f, new Vector3(0.15f, 0f, 0f), 10, 0f, false, false);
+    }
+
+    public void Animate_RodSpin(double beatDuration)
+    {
+        handle.DORotate(new Vector3(0, 0, 360), (float)beatDuration, RotateMode.WorldAxisAdd);
+    }
+
+    public void Animate_ShiftBeatColor(double beatDuration)
+    {
+        beatCircle.DOColor(Color.green, (float)beatDuration).SetEase(Ease.InCubic).OnComplete(() => beatCircle.color = Color.white);
     }
 }
