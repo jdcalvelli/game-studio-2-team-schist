f465121cc48edde64427ecaa65237518c2958476
tommygents <tmaranges+gh@gmail.com>
2023-04-12 20:13:07 -0400

    added some shell functions

--- Assets/Scripts/_HorrorFishingP1/Fishing/FishingViews/FishView.cs
+++ Assets/Scripts/_HorrorFishingP1/Fishing/FishingViews/FishView.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class FishView : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer fishSprite;
+    [SerializeField] private Camera _camera = Camera.main;
 
     public void Animate_FishCaught(Fish fish) {
         Vector3 originalScale = transform.localScale;
@@ -19,5 +20,17 @@ public class FishView : MonoBehaviour
             transform.DOScale(new Vector3(1f, 1f, 1f), 0.5f);
             fishSprite.DOFade(0, 2.5f);
         });
+    }
+
+    // a function to zoom the camera, with the goal of hiding the fishing rod
+    public void Camera_Zoomin()
+    {
+
+    }
+
+    //a function to zoom camera out when the player misses a beat, in order to help them get back on beat
+    public void Camera_Zoomout()
+    {
+
     }
 }
